934f3ccade7daf76d4887adaf0b2d159537285fc
EC2 Default User <ec2-user@ip-172-31-17-230.ap-northeast-1.compute.internal>
2022-06-01 10:17:45 +0800

    [fix] fix shell script

--- crawlFutureDiff.sh
+++ crawlFutureDiff.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /home/ec2-user/project/crawler
 source venv/bin/activate
 python3 -c 'from crawler import getFutureDayDiff; getFutureDayDiff()'
 deactivate
--- crawlStockWeight.sh
+++ crawlStockWeight.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /home/ec2-user/project/crawler
 source venv/bin/activate
 python3 -c 'from crawler import getStockWeight; getStockWeight()'
 deactivate
